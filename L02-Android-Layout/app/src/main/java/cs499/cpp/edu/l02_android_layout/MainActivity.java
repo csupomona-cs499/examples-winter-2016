d819a6bff77b276d2f3791d2601ad22ba5e1a3fa
Yu Sun <yu.sun.cs@gmail.com>
2016-01-13 08:05:38 -0800

    Layout example

---
+++ L02-Android-Layout/app/src/main/java/cs499/cpp/edu/l02_android_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package cs499.cpp.edu.l02_android_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex3_tinder);
+        //setContentView(R.layout.ex2_message_activity);
+    }
+}
